780a278eef80d6ce424883c40e5e3413b0306f39
Aline Regina Pereira Nunes <alinernunes15@gmail.com>
2026-08-20 12:43:03 -0300

    feat(auth): implementar rota de login

--- backend/src/modules/auth/routes/auth.routes.ts
+++ backend/src/modules/auth/routes/auth.routes.ts
@@ -1,1 +1,14 @@
-auth/routes/auth.routes.ts
+import { Router } from "express";
+
+import { LoginController } from "../login/loginController";
+
+const authRoutes = Router();
+
+const loginController = new LoginController();
+
+authRoutes.post(
+  "/login",
+  (req, res) => loginController.handle(req, res)
+);
+
+export { authRoutes };
