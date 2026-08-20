db268717296b54a4417e04ac301d0d5fda3553fb
Aline Regina Pereira Nunes <alinernunes15@gmail.com>
2026-08-20 10:54:22 -0300

    refactor(auth): remover arquivos duplicados

---
+++ backend/src/modules/auth/login/loginController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { LoginSchema } from "../schemas/LoginSchema";
+
+export class LoginController {
+  async handle(
+    req: Request,
+    res: Response
+  ) {
+    const data = LoginSchema.parse(
+      req.body
+    );
+
+    return res.status(200).json({
+      message: "Dados válidos",
+      data,
+    });
+  }
+}
